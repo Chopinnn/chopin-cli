34f5e9a15a15f881a62cd41388c8f1bf2d4cf85d
Chopinnn <353155692@qq.com>
2024-03-19 16:05:21 +0800

    fix: 修复figlet的引入错误

--- src/utils/clone.ts
+++ src/utils/clone.ts
@@ -1,38 +1,48 @@
-
-import simpleGit, {SimpleGit, SimpleGitOptions} from 'simple-git';
+import simpleGit, { SimpleGit, SimpleGitOptions } from 'simple-git';
 import createLogger from 'progress-estimator';
 import chalk from 'chalk';
-const figlet = require('figlet');
+import figlet from 'figlet';
+import standard from "figlet/importable-fonts/Standard.js";
 import log from './log';
+
 // 初始化进度条
 const logger = createLogger({
     spinner: {
         interval: 300,
         frames: ['⠋', '⠙', '⠹', '⠸', '⠼', '⠴', '⠦', '⠧', '⠇', '⠏'].map((item) =>
-            // console.info(item)
             chalk.green(item)
         ),
     },
 });
 
-const goodPrinter = async () => {
-    const data = await figlet('chopinnn-cli');
-    console.log(chalk.rgb(40, 156, 193).visible(data));
-};
+
+figlet.parseFont("Standard", standard);
+figlet.text(
+    "chopinnn-cli",
+    {
+      font: "Standard",
+    },
+    function (err, data) {
+      console.log(chalk.rgb(40, 156, 193).visible(data));
+    }
+  );
 
 const gitOptions: Partial<SimpleGitOptions> = {
     baseDir: process.cwd(), // 当前工作目录
     binary: 'git', // 指定 git 二进制文件路径
     maxConcurrentProcesses: 6, // 最大并发进程数
 };
-export const clone = async (url: string, projectName: string, options: string[]) => {
+export const clone = async (
+    url: string,
+    projectName: string,
+    options: string[]
+) => {
     const git: SimpleGit = simpleGit(gitOptions);
     try {
         await logger(git.clone(url, projectName, options), '代码下载中: ', {
             estimate: 7000, // 预计下载时间
         });
         // 下面就是一些相关的提示
-        goodPrinter();
         console.log();
         console.log(chalk.blueBright(`==================================`));
         console.log(chalk.blueBright(`=== 欢迎使用 chopinnn-cli 脚手架 ===`));
